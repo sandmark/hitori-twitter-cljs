38a69af48ecaa6cfbd1b1a5a94a47b81bf21d4b4
sandmark <sandmark.m@gmail.com>
2019-11-07 00:41:44 +0900

    Add togglable "like!" button

--- src/hitori_twitter/tweet.cljs
+++ src/hitori_twitter/tweet.cljs
@@ -1,10 +1,18 @@
-(ns hitori-twitter.tweet)
+(ns hitori-twitter.tweet
+  (:require [reagent.core :as reagent]))
 
 (defn tweet [{:keys [content icon display-name account-name]}]
-  [:div.tweet
-   [:div.icon-container icon]
-   [:div.body-container
-    [:div.status-display
-     [:span.display-name display-name]
-     [:span.account-name "@" account-name]]
-    [:div.content content]]])
+  (let [state (reagent/atom {:liked false})]
+    (letfn [(toggle-like []
+              (swap! state update :liked not))]
+      (fn []
+        [:div.tweet
+         [:div.icon-container icon]
+         [:div.body-container
+          [:div.status-display
+           [:span.display-name display-name]
+           [:span.account-name "@" account-name]]
+          [:div.content content]
+          [:div.status-action
+           [:span {:on-click toggle-like}
+            (if (:liked @state) "❤️" "♡")]]]]))))
