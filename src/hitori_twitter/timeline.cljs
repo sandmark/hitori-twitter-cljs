821452b9b6f9136a10a50a275fbf8b4f3c48b6b8
sandmark <sandmark.m@gmail.com>
2019-11-08 00:09:34 +0900

    Fix invalid key was assigned to elements of the tweet sequence

--- src/hitori_twitter/timeline.cljs
+++ src/hitori_twitter/timeline.cljs
@@ -2,7 +2,7 @@
   (:require [hitori-twitter.tweet :as tw]))
 
 (defn timeline [{:keys [tweets]}]
-  (letfn [(tweet-with-key [index tweet]
-            [tw/tweet (assoc tweet :key index)])]
-    [:div.timeline
-     (map-indexed tweet-with-key tweets)]))
+  [:div.timeline
+   (->> tweets
+        (map (fn [{:keys [id] :as tweet}]
+               [tw/tweet (assoc tweet :key id)])))])
